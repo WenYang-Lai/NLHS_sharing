98ae260e039c9f6285c4b93618351872c3fb086c
WenYang-Lai <x3639026@gmail.com>
2018-08-17 00:45:03 -0700

    {Completed} babyBof

--- babyBof/main.c
+++ babyBof/main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void open_shell(){
+void magic(){
     system("/bin/sh");
 }
 
-
+void init(){
+    setbuf(stdout, 0);
+    setbuf(stdin, 0);
+}
 int main(int argc, char** argv){
+    init();
     char name[256];
     printf("What's your name: ");
-    scanf("%s\n", name);
-    printf("hello: %s\n");
+    scanf("%s", name);
+    printf("hello: %s\n", name);
 }
